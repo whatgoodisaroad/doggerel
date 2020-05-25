b572bb8c176cbace44889cb1563ff9706f11df47
Wyatt Allen <wyatt.allen0@gmail.com>
2020-05-24 19:39:52 -0700

    Refactored out exec library and documented a little more

---
+++ src/Doggerel/Exec.hs
@@ -0,0 +1,120 @@
+module Doggerel.Exec (execute) where
+
+import Control.Monad.State
+import Data.List (find)
+
+import Doggerel.Ast
+import Doggerel.Eval
+
+-- Is the given identifier already defined in the given state as anything?
+isExistingIdentifier :: Identifier -> ScopeFrame -> Bool
+isExistingIdentifier id (Frame dims units _ assignments)
+  =  id `elem` dims
+  || id `elem` (map fst units)
+  || id `elem` (map fst assignments)
+
+allReferencesAreDefined :: ValueExpression -> (StateT ScopeFrame IO) Bool
+allReferencesAreDefined e = do
+  f <- get
+  return $ all (flip isExistingIdentifier f) $ referencesOfExpr e
+
+-- Execute the given program under an empty scope.
+execute :: Program -> IO ScopeFrame
+execute = executeWith initFrame
+
+executeWith :: ScopeFrame -> Program -> IO ScopeFrame
+executeWith f p
+  = fmap snd
+  $ flip runStateT f
+  $ mapM executeStatement p
+
+-- Execute a single statement inside a state monad carrying the mutable scope.
+executeStatement :: Statement -> (StateT ScopeFrame IO) ()
+
+-- A dimension can be declared so long as its identifier is untaken.
+executeStatement (DeclareDimension dimensionId) = do
+  f@(Frame ds us cs as) <- get
+  if isExistingIdentifier dimensionId f
+    then fail $ "Identifier '" ++ dimensionId ++ "' is already defined."
+    else put $ Frame (dimensionId:ds) us cs as
+
+-- A unit can be declare so long as its identifier is untaken, and, if refers to
+-- a dimension, that dimension is already defined.
+executeStatement (DeclareUnit id maybeDim) = do
+  f@(Frame ds us cs as) <- get
+
+  -- Is the unit ID unused?
+  if isExistingIdentifier id f
+    then fail $ "Identifier '" ++ id ++ "' is already defined."
+    else return ()
+
+  -- If there is a dimension spec, does it refer to an existing dimension?
+  case maybeDim of
+    Nothing -> return ()
+    (Just dim) -> if dim `elem` ds
+      then return ()
+      else fail $ "Reference to undeclared dimension '" ++ dim ++ "'"
+
+  put $ Frame ds ((id, maybeDim):us) cs as
+
+-- A converstion can be defined so long as both units are already defined and
+-- are of the same dimensionality.
+executeStatement (DeclareConversion from to transform) = do
+  f@(Frame ds us cs as) <- get
+
+  if from == to
+    then fail $ "Cannot declare conversion from a unit to itself"
+    else return ()
+
+  let fromUnits = find ((==from).fst) us
+  let toUnits = find ((==to).fst) us
+
+  let noUnitMsg = \u -> "Conversion refers to unkown unit '" ++ u ++ "'"
+  let partialUnitlessMsg = \u -> "Cannot convert dimensionless unit '" ++ u ++ "'"
+
+  case (fromUnits, toUnits) of
+    -- Fail if either side of conversion is unknown units.
+    (Nothing, _) -> fail $ noUnitMsg from
+    (_, Nothing) -> fail $ noUnitMsg to
+
+    -- Fail if units are of different dimensionalities
+    (Just (_, Just fromDim), Just (_, Just toDim)) ->
+      if fromDim == toDim
+        then return ()
+        else fail $ "Cannot declare conversion between units of different "
+          ++ "dimensions: from '" ++ fromDim ++ "' to '" ++ toDim ++ "'"
+    (Just (_, Nothing), _) ->
+      fail $ partialUnitlessMsg from
+    (_, Just (_, Nothing)) ->
+      fail $ partialUnitlessMsg to
+
+  put $ Frame ds us ((from, to, transform):cs) as
+
+-- An assignment can be defined so long as its identifier is untaken and every
+-- reference identifier in its expression tree is already defined.
+executeStatement (Assignment id expr) = do
+  f@(Frame ds us cs as) <- get
+  if isExistingIdentifier id f
+    then fail $ "Identifier '" ++ id ++ "' is already defined"
+    else return ()
+
+  refsDefined <- allReferencesAreDefined expr
+  if not refsDefined
+    then fail $ "Expression refers to unknown identifier"
+    else return ()
+
+  put $ Frame ds us cs ((id, expr):as)
+
+-- A print statement can be executed if every reference identifier in its
+-- expression tree is already defined.
+executeStatement (Print expr _) = do
+  f <- get
+
+  refsDefined <- allReferencesAreDefined expr
+  if not refsDefined
+    then fail $ "Expression refers to unknown identifier"
+    else return ()
+
+  case evaluate f expr of
+    Left err -> fail $ show err
+    Right v -> lift $ putStrLn $ show expr ++ " = " ++ show v
--- src/Doggerel/Main.hs
+++ src/Doggerel/Main.hs
@@ -1,155 +1,47 @@
 module Main where
 
-import Control.Monad.State
-import Data.List (find, sortBy)
-
 import Doggerel.DegreeMap
 import Doggerel.Core
 import Doggerel.Conversion
 import Doggerel.Ast
 import Doggerel.Eval
+import Doggerel.Exec
 
--- Length
-inch = BaseUnit "inch"
-foot = BaseUnit "foot"
-mile = BaseUnit "mile"
-meter = BaseUnit "meter"
-kilometer = BaseUnit "kilometer"
+-- -- Length
+-- inch = BaseUnit "inch"
+-- foot = BaseUnit "foot"
+-- mile = BaseUnit "mile"
+-- meter = BaseUnit "meter"
+-- kilometer = BaseUnit "kilometer"
 
--- Temperature
-fahrenheit = BaseUnit "fahrenheit"
-celsius = BaseUnit "celsius"
-kelvin = BaseUnit "kelvin"
+-- -- Temperature
+-- fahrenheit = BaseUnit "fahrenheit"
+-- celsius = BaseUnit "celsius"
+-- kelvin = BaseUnit "kelvin"
 
--- Time
-second = BaseUnit "second"
-minute = BaseUnit "minute"
-hour = BaseUnit "hour"
-day = BaseUnit "day"
-year = BaseUnit "year"
+-- -- Time
+-- second = BaseUnit "second"
+-- minute = BaseUnit "minute"
+-- hour = BaseUnit "hour"
+-- day = BaseUnit "day"
+-- year = BaseUnit "year"
 
-convertDb :: [Conversion]
-convertDb = [
-    Conversion (LinearTransform 12) foot inch,
-    Conversion (LinearTransform 5280) mile foot,
-    Conversion (LinearTransform 1.60934) mile kilometer,
-    Conversion (LinearTransform 1000) kilometer meter,
-    Conversion (AffineTransForm 1 273.15) celsius kelvin,
-    Conversion (AffineTransForm (9/5) 32) celsius fahrenheit,
-    Conversion (LinearTransform 60) minute second,
-    Conversion (LinearTransform 60) hour minute,
-    Conversion (LinearTransform 24) day hour,
-    Conversion (LinearTransform 365) year day
-  ]
-
--- For testing
+-- convertDb :: [Conversion]
+-- convertDb = [
+--     Conversion (LinearTransform 12) foot inch,
+--     Conversion (LinearTransform 5280) mile foot,
+--     Conversion (LinearTransform 1.60934) mile kilometer,
+--     Conversion (LinearTransform 1000) kilometer meter,
+--     Conversion (AffineTransForm 1 273.15) celsius kelvin,
+--     Conversion (AffineTransForm (9/5) 32) celsius fahrenheit,
+--     Conversion (LinearTransform 60) minute second,
+--     Conversion (LinearTransform 60) hour minute,
+--     Conversion (LinearTransform 24) day hour,
+--     Conversion (LinearTransform 365) year day
+--   ]
 
 u :: String -> DegreeMap BaseUnit
 u = toMap . BaseUnit
-
-(#*), (#/) :: DegreeMap BaseUnit -> DegreeMap BaseUnit -> DegreeMap BaseUnit
-(#*) = multiply
-(#/) = divide
-
-(#) :: Quantity -> String -> Scalar
-m # bu = Scalar m $ toMap $ BaseUnit bu
-
-(##) :: Quantity -> DegreeMap BaseUnit -> Scalar
-(##) = Scalar
-
-mps = u "meter" #/ u "second"
-mips = u "mile" #/ u "second"
-mipm = u "mile" #/ u "minute"
-miph = u "mile" #/ u "hour"
-
-
-
-isExistingIdentifier :: Identifier -> ScopeFrame -> Bool
-isExistingIdentifier id (Frame dims units _ assignments)
-  =  id `elem` dims
-  || id `elem` (map fst units)
-  || id `elem` (map fst assignments)
-
-execute :: Program -> IO ScopeFrame
-execute p
-  = fmap snd
-  $ flip runStateT initFrame
-  $ mapM executeStatement p
-
-executeStatement :: Statement -> (StateT ScopeFrame IO) ()
-executeStatement (DeclareDimension dimensionId) = do
-  f@(Frame ds us cs as) <- get
-  if isExistingIdentifier dimensionId f
-    then fail $ "Identifier '" ++ dimensionId ++ "' is already defined."
-    else put $ Frame (dimensionId:ds) us cs as
-
-executeStatement (DeclareUnit id maybeDim) = do
-  f@(Frame ds us cs as) <- get
-
-  -- Is the unit ID unused?
-  if isExistingIdentifier id f
-    then fail $ "Identifier '" ++ id ++ "' is already defined."
-    else return ()
-
-  -- If there is a dimension spec, does it refer to an existing dimension?
-  case maybeDim of
-    Nothing -> return ()
-    (Just dim) -> if dim `elem` ds
-      then return ()
-      else fail $ "Reference to undeclared dimension '" ++ dim ++ "'"
-
-  put $ Frame ds ((id, maybeDim):us) cs as
-
-executeStatement (DeclareConversion from to transform) = do
-  f@(Frame ds us cs as) <- get
-
-  if from == to
-    then fail $ "Cannot declare conversion from a unit to itself"
-    else return ()
-
-  let fromUnits = find ((==from).fst) us
-  let toUnits = find ((==to).fst) us
-
-  let noUnitMsg = \u -> "Conversion refers to unkown unit '" ++ u ++ "'"
-  let partialUnitlessMsg = \u -> "Cannot convert dimensionless unit '" ++ u ++ "'"
-
-  case (fromUnits, toUnits) of
-    -- Fail if either side of conversion is unknown units.
-    (Nothing, _) -> fail $ noUnitMsg from
-    (_, Nothing) -> fail $ noUnitMsg to
-
-    -- Fail if units are of different dimensionalities
-    (Just (_, Just fromDim), Just (_, Just toDim)) ->
-      if fromDim == toDim
-        then return ()
-        else fail $ "Cannot declare conversion between units of different "
-          ++ "dimensions: from '" ++ fromDim ++ "' to '" ++ toDim ++ "'"
-    (Just (_, Nothing), _) ->
-      fail $ partialUnitlessMsg from
-    (_, Just (_, Nothing)) ->
-      fail $ partialUnitlessMsg to
-
-  put $ Frame ds us ((from, to, transform):cs) as
-
-executeStatement (Assignment id expr) = do
-  f@(Frame ds us cs as) <- get
-  if isExistingIdentifier id f
-    then fail $ "Identifier '" ++ id ++ "' is already defined"
-    else return ()
-
-  flip mapM_ (referencesOfExpr expr) $ \ref ->
-    if not $ isExistingIdentifier ref f
-      then fail $ "Expression refers to unknown identifier: '" ++ ref ++ "'"
-      else return ()
-
-  put $ Frame ds us cs ((id, expr):as)
-
-executeStatement (Print expr _) = do
-  f <- get
-  case evaluate f expr of
-    Left err -> fail $ show err
-    Right v -> lift $ putStrLn $ show expr ++ " = " ++ show v
-
 
 testProgram :: Program
 testProgram = [
@@ -176,7 +68,9 @@ testProgram = [
     Print (Reference "x") Nothing,
     Print (Reference "z") Nothing,
     Print (Reference "w") Nothing,
-    Print (BinaryOperatorApply Add (Reference "w") (BinaryOperatorApply Multiply (Reference "x") (Reference "z"))) Nothing
+    Print (BinaryOperatorApply Add (Reference "w") (BinaryOperatorApply Multiply (Reference "x") (Reference "z"))) Nothing,
+
+    Print (Reference "doesNotExist") Nothing
 
 
     -- Print (Reference "x") Nothing,

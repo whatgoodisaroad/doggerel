6d02ee2ed5ee5d4ce53ed9c36808915406e61ba6
Wyatt Allen <wyatt.allen0@gmail.com>
2020-06-21 09:02:25 -0700

    Add tests and documentation for Doggerel.Output

--- src/Doggerel/Output.hs
+++ src/Doggerel/Output.hs
@@ -9,20 +9,27 @@ import Doggerel.Ast
 import Doggerel.Core
 import Doggerel.DegreeMap
 
+-- With the MultiLineFractions option, if any component of the vector have units
+-- of negative degree, then print on three lines in a fraction style. Otherwise
+-- print non fractional vectors on one line.
 data PrintOption = MultiLineFractions
   deriving (Eq, Ord)
 
+-- Pretty print the expression and the resulting vector value that in the
+-- desired style.
 prettyPrint :: Set PrintOption -> ValueExpression -> Vector -> [String]
 prettyPrint opts expr vec
   = if MultiLineFractions `elem` opts && anyComponentIsFraction vec
     then multiline expr vec
     else [oneLine expr vec]
 
+-- Does any component of the given vector have units of negative degree.
 anyComponentIsFraction :: Vector -> Bool
 anyComponentIsFraction v = flip any (vectorAsFractions v) $ \f -> case f of
   Left _ -> False
   Right _ -> True
 
+-- Pretty print in the multiline fraction style.
 multiline :: ValueExpression -> Vector -> [String]
 multiline expr vec = [
     topBot ++ "   " ++ openBraceTop ++ " " ++ a ++ " " ++ closeBraceTop,
@@ -42,15 +49,21 @@ closeBraceTop = "⎫"
 closeBraceMid = "⎬"
 closeBraceBot = "⎭"
 
+-- Print the expression for multiline style as a tuple of the printed expression
+-- and the spaces to print above and below it for proper alignment.
 showMultilineExpr :: ValueExpression -> (String, String)
 showMultilineExpr e = (es, topBot)
   where
     es = show e
     topBot = take (length es) $ repeat ' '
 
+-- Pretty print in the one-line style.
 oneLine :: ValueExpression -> Vector -> String
 oneLine expr vec = show expr ++ " = " ++ show vec
 
+-- Given a vector component either represented as a non-fractional scalar, or as
+-- a tuple of quantity, numerator units and denominator units, express the
+-- printed result as three lines of text of equal length.
 showComponent ::
      Either Scalar (Quantity, Units, Units)
   -> (String, String, String)
@@ -73,6 +86,9 @@ showComponent (Right (q, num, den)) = (top, mid, bottom)
       else dens
     mid = take ((length top) `max` (length bottom)) $ repeat '─'
 
+-- Given a triplet of separator strings and a list of printed component triples
+-- concatenate each together (with the corresponding separator interspersed)
+-- into a resulting triple.
 concatLists ::
      (String, String, String)
   -> [(String, String, String)]
@@ -86,6 +102,10 @@ concatLists sep@(sepTop, sepMid, sepBot) ((a, b, c):d)
         more = not $ null d
         (e, f, g) = concatLists sep d
 
+-- Given a vector, give a list of Either values, each representing a component
+-- of the vector. If a component has no fractional part, it's represented as a
+-- scalar, but if it does have a fractional part, the parts of the component are
+-- separated into a truple of quantity, numerator units and denominator units.
 vectorAsFractions :: Vector -> [Either Scalar (Quantity, Units, Units)]
 vectorAsFractions (Vector m) = map toEither $ assocs m
   where
---
+++ tests/Doggerel/OutputTests.hs
@@ -0,0 +1,51 @@
+module Main where
+
+import Data.Set (Set, empty, fromList)
+import Doggerel.Ast;
+import Doggerel.Core
+import Doggerel.DegreeMap
+import Doggerel.Output
+import System.Exit (exitFailure)
+import Test.HUnit
+
+u :: String -> Units
+u = toMap . BaseUnit
+
+simpleOneline = TestCase $ assertEqual "simple one line print" expected actual
+  where
+    expected = ["foo = {5.0 bar·baz⁻¹}"]
+    actual = prettyPrint empty (Reference "foo")
+      $ scalarToVector
+      $ Scalar 5.0
+      $ u "bar" `divide` u "baz"
+
+simpleMultiline = TestCase $ assertEqual "simple multi line print" expected actual
+  where
+    expected = [
+        "      ⎧ 5.0 bar ⎫",
+        "foo = ⎨ ─────── ⎬",
+        "      ⎩     baz ⎭"
+      ]
+    actual = prettyPrint (fromList [MultiLineFractions]) (Reference "foo")
+      $ scalarToVector
+      $ Scalar 5.0
+      $ u "bar" `divide` u "baz"
+
+multilineWithoutFraction
+  = TestCase $ assertEqual "mnultiline but no fraction" expected actual
+  where
+    expected = ["foo = {5.0 bar·baz}"]
+    actual = prettyPrint (fromList [MultiLineFractions]) (Reference "foo")
+      $ scalarToVector
+      $ Scalar 5.0
+      $ u "bar" `multiply` u "baz"
+
+unitTests = [
+    simpleOneline,
+    simpleMultiline,
+    multilineWithoutFraction
+  ]
+
+main = do
+  count <- runTestTT (TestList unitTests)
+  if failures count > 0 then exitFailure else return ()
